2e206f254a230aa91135468f59a7407c7d1d42ec
LeonardoRicardo <leonardojlleo@gmail.com>
2026-06-25 23:40:33 -0300

    Desafio 005

---
+++ 04-Sucessor-e-Antecessor.py
@@ -0,0 +1,3 @@
+#Faça um programa que leia um número inteiro e mostre na tela o seu sucessor e seu antecessor
+n=int(input("Digite um número inteiro: "))
+print(f"o número digitado foi {n}, seu antecessor é {n-1} e seu sucessor é {n+1}")
