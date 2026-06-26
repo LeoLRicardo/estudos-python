dbc5049f83a2f6e2340d09bf19f1761ecfe9503b
LeonardoRicardo <leonardojlleo@gmail.com>
2026-06-25 23:56:30 -0300

    Alterei todos os nomes para estar de acordo com os exercícios do guanabara e adicionei o exercício 006

---
+++ 01-LogicaBasica/006-dobro.py
@@ -0,0 +1,5 @@
+#Crie um algoritmo que leia um número e mostre o seu dobro, triplo e raiz quadrada
+n= int(input("insira aqui um número inteiro: "))
+print(f"Esse é o dobro do seu número:\n{n*2} ")
+print(f"Esse é o triplo do seu número:\n{n*3} ")
+print(f"Essa é a raiz quadrada do seu número:\n{n**(1/2)} ")
